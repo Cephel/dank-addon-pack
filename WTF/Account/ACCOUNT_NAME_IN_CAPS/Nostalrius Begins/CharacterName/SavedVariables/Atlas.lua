2e3b9656bb402f6eb4e1d9de2f74e6820c48e889
Cephei <cepheidev+git@gmail.com>
2015-09-11 01:19:43 +0200

    More config files

---
+++ WTF/Account/ACCOUNT_NAME_IN_CAPS/Nostalrius Begins/CharacterName/SavedVariables/Atlas.lua
@@ -0,0 +1,18 @@
+
+AtlasOptions = {
+	["AtlasMapName"] = true,
+	["AtlasReplaceWorldMap"] = false,
+	["AtlasAlpha"] = 1,
+	["AtlasButtonRadius"] = 78,
+	["AtlasAutoSelect"] = true,
+	["AtlasRightClick"] = false,
+	["AtlasClamped"] = true,
+	["AtlasAcronyms"] = true,
+	["AtlasType"] = 1,
+	["AtlasButtonShown"] = true,
+	["AtlasVersion"] = "1.8.5a |cFFFF8080PRE-TBC|r",
+	["AtlasButtonPosition"] = 15,
+	["AtlasZone"] = 3,
+	["AtlasScale"] = 1,
+	["AtlasLocked"] = false,
+}
---
+++ WTF/Account/ACCOUNT_NAME_IN_CAPS/SavedVariables/AtlasLoot.lua
@@ -0,0 +1,21 @@
+
+AtlasLootOptions = {
+	["SelectedZone"] = 1,
+	["SelectedZoneText"] = "Blackrock Depths",
+	["MinimapButtonAngle"] = 0,
+	["ItemIDs"] = false,
+	["Opaque"] = false,
+	["SafeLinks"] = true,
+	["EquipCompare"] = true,
+	["DefaultTT"] = true,
+	["AllLinks"] = false,
+	["ItemSyncTT"] = false,
+	["SelectedBoss"] = 1,
+	["SelectedType"] = 1,
+	["SilentMode"] = true,
+	["MinimapButton"] = false,
+	["LootlinkTT"] = false,
+}
+AtlasLootVersion = "12102"
+AtlasLootSavedTooltips = {
+}
