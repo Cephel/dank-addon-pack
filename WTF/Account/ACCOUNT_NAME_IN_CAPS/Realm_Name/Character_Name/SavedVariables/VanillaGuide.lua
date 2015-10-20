88247f7835eb5065b60ad81c6e256210e29fb650
Cephei <cepheidev+git@gmail.com>
2015-10-20 19:55:56 +0200

    Added vanillaguide layouting

---
+++ WTF/Account/ACCOUNT_NAME_IN_CAPS/Realm_Name/Character_Name/SavedVariables/VanillaGuide.lua
@@ -0,0 +1,30 @@
+
+VanillaGuideDBPC = {
+	["global"] = {
+		["UI"] = {
+			["MainFrameAnchor"] = {
+				["sTo"] = "TOPLEFT",
+				["nX"] = 975,
+				["sFrom"] = "TOPLEFT",
+				["nY"] = -475,
+			},
+			["Locked"] = true,
+			["ScrollFrameVisible"] = false,
+			["MainFrameSize"] = {
+				["nWidth"] = 320.0000043767834,
+				["nHeight"] = 320.0000043767834,
+			},
+			["MainFrameColor"] = {
+				["nR"] = 0.1098039215686275,
+				["nA"] = 0,
+				["nB"] = 0.1098039215686275,
+				["nG"] = 0.1098039215686275,
+			},
+		},
+		["MetaMap"] = {
+			["BWPPresence"] = true,
+			["Presence"] = true,
+			["NotesPresence"] = true,
+		},
+	},
+}
