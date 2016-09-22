577ad7fc1a2a45af6fc03c1e9cf7e8142e2d6f86
Cephei <cepheidev+git@gmail.com>
2016-09-22 17:00:47 +0200

    Config updates

---
+++ WTF/Account/ACCOUNT_NAME_IN_CAPS/Realm_Name/Character_Name/SavedVariables/AtlasLoot.lua
@@ -0,0 +1,28 @@
+
+AtlasLootCharDB = {
+	["DefaultTT"] = true,
+	["ItemSyncTT"] = false,
+	["AutoQuery"] = false,
+	["ItemIDs"] = false,
+	["LastBossText"] = "Taragaman the Hungerer",
+	["Opaque"] = false,
+	["SafeLinks"] = true,
+	["SearchResult"] = {
+	},
+	["PartialMatching"] = true,
+	["MinimapButtonRadius"] = 78,
+	["WishList"] = {
+	},
+	["LootlinkTT"] = false,
+	["HidePanel"] = false,
+	["ItemSpam"] = true,
+	["EquipCompare"] = true,
+	["AllLinks"] = false,
+	["QuickLooks"] = {
+	},
+	["AtlasLootVersion"] = "40605",
+	["MinimapButtonPosition"] = 315,
+	["FirstTime"] = false,
+	["MinimapButton"] = false,
+	["LastBoss"] = "RFCTaragaman",
+}
