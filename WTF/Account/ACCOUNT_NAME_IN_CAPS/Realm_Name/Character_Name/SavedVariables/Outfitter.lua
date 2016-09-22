2e165f95e87ee6c7ca16e9aaef1955eef2687637
Cephei <cepheidev+git@gmail.com>
2016-09-22 17:02:19 +0200

    More config changes

---
+++ WTF/Account/ACCOUNT_NAME_IN_CAPS/Realm_Name/Character_Name/SavedVariables/Outfitter.lua
@@ -0,0 +1,7 @@
+
+gOutfitter_Settings = {
+	["Options"] = {
+		["MinimapButtonAngle"] = -1.5708,
+		["HideMinimapButton"] = true,
+	},
+}
---
+++ WTF/Account/ACCOUNT_NAME_IN_CAPS/SavedVariables/LVBM_API.lua
@@ -0,0 +1,10 @@
+
+LVBM_SavedVars = {
+	["LVBM"] = {
+		["MinimapButton"] = {
+			["Enabled"] = false,
+			["Position"] = 225,
+			["Radius"] = 78.09999999999999,
+		},
+	},
+}
