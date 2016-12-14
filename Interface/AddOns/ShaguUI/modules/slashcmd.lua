f5fce141ed7f4eda13820a92e46e4b49c1ecceb4
Cephei <cepheidev+git@gmail.com>
2016-12-14 01:33:34 +0100

    Remove ShaguUI in preparation for pFui

--- Interface/AddOns/ShaguUI/modules/slashcmd.lua
+++
@@ -1,9 +0,0 @@
-SLASH_OPENGM1, SLASH_OPENGM2 = '/gm', '/support';
-function SlashCmdList.OPENGM(msg, editbox)
-  ToggleHelpFrame(1)
-end
-
-SLASH_RELOAD1, SLASH_RELOAD2 = '/rl', '/reload';
-function SlashCmdList.RELOAD(msg, editbox)
-  ReloadUI()
-end
