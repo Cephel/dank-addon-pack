33236df307d6e58b4f3711fe16607c3ee8634354
Cephei <cepheidev+git@gmail.com>
2016-12-30 05:10:57 +0100

    Added VanillaGuide config

---
+++ WTF/Account/ACCOUNT_NAME_IN_CAPS/Realm_name/Character_Name/SavedVariables/VanillaGuide.lua
@@ -0,0 +1,21 @@
+
+VanillaGuideDBPC = {
+	["global"] = {
+		["CharInfo"] = {
+		},
+		["UI"] = {
+			["MainFrameAnchor"] = {
+				["sTo"] = "TOPLEFT",
+				["nX"] = 1016,
+				["sFrom"] = "TOPLEFT",
+				["nY"] = -461,
+			},
+			["Locked"] = true,
+			["ScrollFrameVisible"] = false,
+			["MainFrameSize"] = {
+				["nWidth"] = 320,
+				["nHeight"] = 420,
+			},
+		},
+	},
+}
